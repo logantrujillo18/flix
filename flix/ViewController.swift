f6e6ecda18b819d33ba9b8ca6fa369497e6629ae
Logan Trujillo <2001221633@student.csn.edu>
2021-02-02 18:32:36 -0800

    Initial Commit

---
+++ flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flix
+//
+//  Created by user190009 on 2/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
