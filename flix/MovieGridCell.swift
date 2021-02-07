42c5fbd30f257b5da00d75990e8c76b116d27d73
Logan Trujillo <2001221633@student.csn.edu>
2021-02-06 16:03:27 -0800

    assignment 2 start

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by user190009 on 2/6/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIView!
+    
+    
+}
